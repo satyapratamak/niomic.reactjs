c902f7774da81a783214007d9ad6a642d2afb6dd
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2020-12-13 00:28:29 +0700

    COMMIT : vid 3 - 29

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,31 @@ class Main extends Component{
     super(props);
 
     this.state = {
-      title:"Menu Makanan"
+      title:"Menu Makanan",
+      title2: "Menu Minuman"
     }
 
     this.ubahData = this.ubahData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  ubahData(){
+  /*ubahData(){
     this.setState({title:"Pilih Menu"})
+  }*/
+
+  ubahData(){
+    this.setState( (state, props) => {
+      return {
+        title: state.title2,
+        title2: state.title
+      };
+
+    });
+
+  }
+
+  handleChange(){
+    console.log("Jalan");
   }
 
   render(){
@@ -23,7 +40,11 @@ class Main extends Component{
 
       <div>
         <h3> {this.state.title} </h3>
+        <h2> {this.state.title2} </h2>
         <button onClick={this.ubahData}> UBAH DATA </button>
+        <br/>
+          <br/>
+        <input type="text" onChange={this.handleChange} />
       </div>
     );
   }
